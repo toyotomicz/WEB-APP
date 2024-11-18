4b2dbca8b3ba53be6e0d8e655d21af805f0553c9
Jiří Joska <cz.terror@gmail.com>
2024-11-18 11:03:43 +0100

    smazani ostatniho

--- semestralka/app/controllers/OrderController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class OrderController {
-    public function showOrders() {
-        require 'views/orders.php';
-    }
-}
-?>
--- semestralka/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="bg-dark text-light py-3 mt-auto">
-    <div class="container text-center">
-        <p class="mb-1">Adresa: Pizzerie u Hvězdy, Hlavní 123, Praha 1</p>
-        <p class="mb-1">Telefon: +420 123 456 789</p>
-        <p class="mb-0">© 2024 Pizzerie u Hvězdy. Všechna práva vyhrazena.</p>
-    </div>
-</footer>
