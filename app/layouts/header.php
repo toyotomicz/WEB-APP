7ce4f49aa0deda87bb1a8b4b4f15a97229908fa4
Jiří Joska <cz.terror@gmail.com>
2024-11-27 16:51:55 +0100

    zmena menu

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -4,7 +4,7 @@ $cart_count = isset($_SESSION['cart']) ? array_sum(array_column($_SESSION['cart'
 
 // Předpokládáme, že informace o uživateli jsou uloženy v $_SESSION
 $username = isset($_SESSION['user']['username']) ? $_SESSION['user']['username'] : null;
-$role = isset($_SESSION['user']['role']) ? $_SESSION['user']['role'] : null;
+$role = isset($_SESSION['user']['role']) ? (int)$_SESSION['user']['role'] : null;
 ?>
 
 <nav class="navbar navbar-expand-xxl navbar-dark" style="background-color: #34495e;">
@@ -14,10 +14,11 @@ $role = isset($_SESSION['user']['role']) ? $_SESSION['user']['role'] : null;
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="menu">Menu</a>
-            </li>
             <?php if (!$username): ?>
+                <!-- Pokud není uživatel přihlášen -->
+                <li class="nav-item">
+                        <a class="nav-link" href="menu">Menu</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="login">Přihlášení</a>
                 </li>
@@ -25,18 +26,37 @@ $role = isset($_SESSION['user']['role']) ? $_SESSION['user']['role'] : null;
                     <a class="nav-link" href="register">Registrace</a>
                 </li>
             <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="orders">Objednávky</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add-pizza">Pizzy</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add-cook">Přidat kuchaře</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add-admin">Přidat admina</a>
-                </li>
+                <!-- Navigace podle role -->
+                <?php if ($role === 1): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="orders">Objednávky</a>
+                    </li>
+                <?php elseif ($role === 2 || $role === 3 || $role === 4): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cart">
+                            Košík <span class="badge bg-danger rounded-pill"><?php echo $cart_count; ?></span>
+                        </a>
+                    </li>
+                <?php endif; ?>
+
+                <?php if ($role === 3 || $role === 4): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="orders">Objednávky</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="add-pizza">Pizzy</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="add-cook">Přidat kuchaře</a>
+                    </li>
+                <?php endif; ?>
+
+                <?php if ($role === 4): ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="add-admin">Přidat admina</a>
+                    </li>
+                <?php endif; ?>
+
                 <li class="nav-item">
                     <span class="navbar-text text-light">
                         Přihlášen jako: <?php echo htmlspecialchars($username); ?> (Role: <?php echo htmlspecialchars($role); ?>)
@@ -46,11 +66,6 @@ $role = isset($_SESSION['user']['role']) ? $_SESSION['user']['role'] : null;
                     <a class="nav-link" href="logout">Odhlásit se</a>
                 </li>
             <?php endif; ?>
-            <li class="nav-item">
-                <a class="nav-link" href="cart">
-                    Košík <span class="badge bg-danger rounded-pill"><?php echo $cart_count; ?></span>
-                </a>
-            </li>
         </ul>
     </div>
 </nav>
